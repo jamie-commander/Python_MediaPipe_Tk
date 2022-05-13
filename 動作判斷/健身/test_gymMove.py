04fde289e4ef0d8529d4ffb57cbeffb5344b0714
JorJor <67410120+hellojor@users.noreply.github.com>
2022-05-13 16:24:36 +0800

    Add files via upload

---
+++ 動作判斷/健身/test_gymMove.py
@@ -0,0 +1,79 @@
+# 二頭彎舉 curl
+import cv2
+import mediapipe as mp
+from gymMove import *
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+out = None
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = cv2.flip(frame, 1)
+        # convert color BGR to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        result = pose.process(image)
+
+        # convert color RGB to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Extract Landmarks
+        try:
+            landmarks = result.pose_landmarks.landmark
+            out = reverse_crunch(landmarks, mp_pose)
+        except:
+            pass
+
+        if out != None:
+            (leftcounter, rightcounter, leftstage, rightstage) = out
+            # status box
+            cv2.rectangle(image, (0, 0), (250, 73), (245, 117, 16), -1)
+            cv2.rectangle(image, (390, 0), (640, 73), (245, 117, 16), -1)
+
+            # LEFT REPS
+            cv2.putText(image, 'REPS', (15, 12),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(leftcounter), (10, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # LEFT STAGE
+            cv2.putText(image, 'STAGE', (85, 12),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(leftstage), (90, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # RIGHT REPS
+            cv2.putText(image, 'REPS', (405, 12),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(rightcounter), (400, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            
+            # RIGHT STAGE
+            cv2.putText(image, 'STAGE', (475, 12),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(rightstage), (480, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+
+        # render detection
+        mp_drawing.draw_landmarks(image, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        
+        cv2.imshow('Mediapipe read', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    else:
+        print('NOT DETECT CAMERA')
+  
+
+cap.release()
+cv2.destroyAllWindows()
